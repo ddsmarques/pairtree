c4dba07d4b8e11509d5dbeeb265917dd871de77a
Daniel Marques <danielsmarques7@gmail.com>
2015-12-04 21:09:33 -0200

    Adding config example file in lua.

---
+++ example.lua
@@ -0,0 +1,15 @@
+
+dataset = "C:/Users/Daniel/Documents/DataSets/xor_fake.csv"
+output = "folder"
+
+tree = {
+	treeType = "pine",
+	height = 2,
+	solver = "INTEGER"
+}
+--[[
+tree = {
+	treeType = "greedy",
+	height = 2
+}
+--]]
